152ac39988fc04b107dd099ad129b64bae6ef4b9
Ranjay_Define@define <ran2492002@gmail.com>
2025-09-30 00:24:52 +0530

    Learning Spinners in Android

---
+++ SPINNERS/app/src/main/java/com/android/spinners/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.android.spinners
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var spinner : Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    spinner = findViewById(R.id.spinner)
+    val list = listOf("Apple","Orange","Banana","Grapes","Dragon_Fruit")
+    val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,list)
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    spinner.adapter = adapter
+
+    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) {
+            val selectedItem = parent?.getItemAtPosition(position).toString()
+            Toast.makeText(this@MainActivity,"$selectedItem is selected", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
