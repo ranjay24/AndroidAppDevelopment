d12ca8575dba811accc1ae7c29fdf4e274cbd328
Ranjay_Define@define <ran2492002@gmail.com>
2025-10-13 00:27:10 +0530

    Leaning to use view Binding in Android

---
+++ ViewBinding/app/src/main/java/com/android/viewbinding/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.android.viewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view  = mainBinding.root
+        enableEdgeToEdge()
+        setContentView(view)
+
+        mainBinding.buttonOk.setOnClickListener {
+            mainBinding.textViewResult.text = mainBinding.editText.text.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
