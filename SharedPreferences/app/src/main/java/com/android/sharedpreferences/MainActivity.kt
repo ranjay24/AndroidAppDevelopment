6282206f9c23184da4fb620c37773e03016d662d
Ranjay_Define@define <ran2492002@gmail.com>
2025-10-13 23:56:28 +0530

    Learn how to save primitive data in Android using SharedPreference

---
+++ SharedPreferences/app/src/main/java/com/android/sharedpreferences/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.android.sharedpreferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var userName : EditText
+    lateinit var userMessage : EditText
+    lateinit var counter : Button
+    lateinit var remember : CheckBox
+
+    var count : Int = 0
+
+    var name : String? = null
+    var message : String? = null
+    var isChecked : Boolean? = null
+    lateinit var sharedPreferences : SharedPreferences
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        userName = findViewById(R.id.editTextName)
+        userMessage = findViewById(R.id.editTextMessage)
+        counter = findViewById(R.id.button)
+        remember = findViewById(R.id.checkBox)
+
+       counter.setOnClickListener {
+           count++
+           counter.setText(""+count)
+       }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveDate()
+    }
+
+    fun saveDate(){
+        sharedPreferences = this.getSharedPreferences("saveData", Context.MODE_PRIVATE)
+        name = userName.text.toString()
+        message = userMessage.text.toString()
+        isChecked = remember.isChecked
+
+        val editor = sharedPreferences.edit()
+
+        editor.putString("key name" , name)
+        editor.putString("Key message", message)
+        editor.putInt("key count",count)
+        editor.putBoolean("key boolean",isChecked!!)
+
+        editor.apply()
+        Toast.makeText(applicationContext,"App Data Saved" , Toast.LENGTH_SHORT).show()
+    }
+}
