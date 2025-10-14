e692d5d3f04b6c4d9d4df6f609b6aeb9751b1a2f
Ranjay_Define@define <ran2492002@gmail.com>
2025-10-15 00:17:50 +0530

    Implemented checkBox for learning

--- MultipleLanguage/app/src/main/java/com/android/multiplelanguage/MainActivity.kt
+++ MultipleLanguage/app/src/main/java/com/android/multiplelanguage/MainActivity.kt
@@ -2,6 +2,8 @@ package com.android.multiplelanguage
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,15 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var show : Button
+
+
+
+    //Radio Button Learning Implmentation
+    lateinit var display : TextView
+    lateinit var learning : CheckBox
+    lateinit var learned : CheckBox
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +35,33 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext,R.string.toast, Toast.LENGTH_LONG).show()
         }
 
+
+
+
+        //Radio Button Learning Implmentation
+        display = findViewById(R.id.display)
+        learning = findViewById(R.id.learningChecked)
+        learned = findViewById(R.id.learnedChecked)
+
+        learning.setOnClickListener {
+            if(learning.isChecked){
+                display.text = "Still Learning"
+                learned.isChecked = false
+            }
+            else{
+                display.text = "What is the Progress ?"
+            }
+
+
+        }
+        learned.setOnClickListener {
+            if (learned.isChecked){
+                display.text = "Learned"
+                learning.isChecked  = false
+            } else{
+                display.text = "What is the Progress ?"
+            }
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
